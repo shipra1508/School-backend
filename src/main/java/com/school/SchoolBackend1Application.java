9c05cfdca321e3a24e49ef9a1729bee5373618af
Shipra <shiprashikta@gmail.com>
2025-06-06 18:19:24 -0500

    basic spring application

---
+++ src/main/java/com/school/SchoolBackend1Application.java
@@ -0,0 +1,13 @@
+package com.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolBackend1Application.class, args);
+	}
+
+}
